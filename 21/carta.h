6f93ed26afcd37366e27eccaf6016ffa8448f52c
Kevin Acevedo <keviinar51@gmail.com>
2022-07-22 15:46:32 -0600

    Starting to work on lista-enlazada branch

---
+++ 21/carta.h
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cFiles/file.h to edit this template
+ */
+
+/* 
+ * File:   carta.h
+ * Author: kevin
+ *
+ * Created on July 22, 2022, 3:45 PM
+ */
+
+#ifndef CARTA_H
+#define CARTA_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* CARTA_H */
+
