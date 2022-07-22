2fa944cb4a7e517c5e199bc09c0ee0994eb0d921
Kevin Acevedo <keviinar51@gmail.com>
2022-07-22 14:40:12 -0600

    Initializing the project structure in C

---
+++ 21/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: kevin
+ *
+ * Created on July 22, 2022, 2:32 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
